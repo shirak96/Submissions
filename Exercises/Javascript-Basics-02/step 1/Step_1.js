cb02ee364f15d2a8174782f8c0a0bed6a351bb94
Shirak <shirak.kevorkian@gmail.com>
2019-05-21 11:17:29 +0300

    Step_1.js

---
+++ Exercises/Javascript-Basics-02/step 1/Step_1.js
@@ -0,0 +1,9 @@
+var img = document.querySelector("#image1");
+
+img.onmouseover = function() {
+    img.src="images/image1_2.jpg"
+}
+img.onmouseout = function() {
+   img.src="images/image1.jpg"
+}
+
