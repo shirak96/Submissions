80508a80a69f8b8c2026004010878d3a22ebaac8
Shirak <shirak.kevorkian@gmail.com>
2019-05-23 16:21:04 +0300

    Step_3.js

---
+++ Exercises/Javascript-Basics-03/step 3/Step_3.js
@@ -0,0 +1,13 @@
+function grb(a) {
+   if (a == "green"){
+       document.querySelector("p").style.color = "green";
+   }
+   if  (a == "red"){
+    document.querySelector("p").style.color = "red";
+}
+   if  (a == "blue"){
+    document.querySelector("p").style.color = "blue";
+}
+
+
+}
