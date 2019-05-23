fe93656cea1fe4ac60c2246f28d38993ccde087b
Shirak <shirak.kevorkian@gmail.com>
2019-05-23 06:29:44 +0300

    Step_4.js

---
+++ Exercises/Javascript-Basics-02/step 4/Step_4.js
@@ -0,0 +1,12 @@
+var rest = document.querySelector("button")
+    
+rest.onclick = function() {
+    var yon ;
+    if (confirm("yes or no"))
+    
+    {
+    y=document.querySelector("#name").value =" ";
+    document.querySelector("#surname").value =" ";
+    document.querySelector("#city").value =" ";
+}
+}
