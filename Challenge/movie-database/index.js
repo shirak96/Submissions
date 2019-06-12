70d6d1541eb62b3dd292af76625c442bb2a2d147
Shirak <shirak.kevorkian@gmail.com>
2019-06-12 12:53:25 +0300

    movie-db-step5

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -1,5 +1,8 @@
-var express = require('express');
+
+
+var express = require('express'); 
 var app = express();
+
 
 
 app.get('/', function (req, res) {
@@ -49,5 +52,43 @@ else
         message:"you have to provide a search"
     })
 });
+
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+]
+
+
+app.get('/movies/create', function (req, res) {
+    res.status(200).send({
+        status:200, 
+        message:"cre"
+    })
+});
+
+app.get('/movies/read', function (req, res) {
+    res.status(200).send({
+        status:200, 
+        data:movies
+    })
+});
+
+app.get('/movies/update', function (req, res) {
+    res.status(200).send({
+        status:200, 
+        message:"upd"
+    })
+});
+
+app.get('/movies/delete', function (req, res) {
+    res.status(200).send({
+        status:200, 
+        message:"del"
+    })
+});
+
+
 
 app.listen(3000)
