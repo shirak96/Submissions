93650fcbd5b6c9abd5bda03801a4c85a14d4a821
Shirak <shirak.kevorkian@gmail.com>
2019-06-12 09:57:08 +0300

    movie-db-step3

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -5,5 +5,19 @@ var app = express();
 app.get('/', function (req, res) {
     res.send('<h1>Okay</h1>');
 })
+app.get('/test', function (req, res) {
+    res.status(200).send({
+        status:200, 
+        message:"ok"
+    })
+});
 
+app.get('/time', function (req, res) {
+    var today = new Date();
+var time = today.getHours() + ":" + today.getSeconds();
+    res.status(200).send({
+        status:200, 
+        message:time
+    })
+});
 app.listen(3000)
